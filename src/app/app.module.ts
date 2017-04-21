2335c2df94045f55fe5fa89d173a80306f73eab2
Виктор <Viktor@VbuyakovMacBook.local>
2017-04-21 20:41:24 +0300

    create comnponts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { CategoriesListComponent } from './categories-list/categories-list.component';
+import { CategoryEditorComponent } from './category-editor/category-editor.component';
+import { ProductEditorComponent } from './product-editor/product-editor.component';
+import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    CategoriesListComponent,
+    CategoryEditorComponent,
+    ProductEditorComponent,
+    DeleteConfirmComponent
   ],
   imports: [
     BrowserModule,
